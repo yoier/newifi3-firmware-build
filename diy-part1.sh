379b7430d831ca88ac9894215dadf60061aa8fc1
Yoier <huaimingdeng@gmail.com>
2025-04-20 02:59:20 +0800

    test

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,3 +9,12 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 # This script was created by yoier <https://github.com/yoier/r3s-firmware-build/blob/main/diy-part1.sh>
+mkdir pkgLean && cd pkgLean
+git init
+git remote add origin https://github.com/coolsnowwolf/lede.git
+git sparse-checkout init --cone
+git sparse-checkout set package/lean
+git pull origin master
+cd ../
+cp -ar pkgLean/package/lean package
+rm -rf pkgLean
