3e74a21d64bbb98191ca47bc9c36f4771ce80e6e
Yoier <huaimingdeng@gmail.com>
2025-04-19 17:01:50 +0800

    first commit

---
+++ diy-part2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+# This script was created by yoier <https://github.com/yoier/r3s-firmware-build/blob/main/diy-part2.sh>
+nowDate=`TZ="UTC-8" date "+%Y-%m-%d %H:%M:%S"`
+sed -i "/return table/i table.appendChild(E('tr', { 'class': 'tr' }, [E('td', { 'class': 'td left', 'width': '33%' }, ['仓库地址 | 构建时间']),E('td', { 'class': 'td left' }, [E('a', { 'href': 'https://github.com/yoier/newifi3-firmware-build', 'target': '_blank' }, 'Powered by yoier/newifi3-firmware-build | $nowDate')])]));" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
